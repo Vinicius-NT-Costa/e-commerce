0d19628d14ace24b527bdf6a5ad3f04884eb412a
Vinicius Costa <vinicius.nt.costa@gmail.com>
2025-08-21 17:18:03 -0300

    Add project files.

---
+++ UserService/Infrastructure/Configuration/SerilogConfig.cs
@@ -0,0 +1,12 @@
+﻿using Serilog;
+
+namespace UserService.Infrastructure.Configuration;
+
+public static class SerilogConfig
+{
+    public static void ConfigureLogger(IServiceProvider services, LoggerConfiguration loggerConfig, WebApplicationBuilder builder)
+    {
+        loggerConfig.ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(services);
+    }
+}
---
+++ UserService/Program.cs
@@ -0,0 +1,46 @@
+using Serilog;
+using UserService.Infrastructure.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up microservice...");
+
+try
+{
+    builder.Services.AddSerilog((services, loggerConfig) =>
+        SerilogConfig.ConfigureLogger(services, loggerConfig, builder));
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseSerilogRequestLogging();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
+
